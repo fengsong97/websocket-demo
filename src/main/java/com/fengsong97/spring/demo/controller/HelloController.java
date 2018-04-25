279fbf3fa0eca6b1291ed344996a4131a514027c
冯松 <fengsong97@qq.com>
2018-04-25 17:06:44 +0800

    添加 post 方法

--- src/main/java/com/fengsong97/spring/demo/controller/HelloController.java
+++ src/main/java/com/fengsong97/spring/demo/controller/HelloController.java
@@ -2,11 +2,17 @@ package com.fengsong97.spring.demo.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.fengsong97.spring.demo.entity.Book;
+import com.fengsong97.spring.demo.request.BookCreateRequest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.mapstruct.Context;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 /**
  * 创建人 fengsong
@@ -19,7 +25,7 @@ public class HelloController {
 
     @ApiOperation(value = "获得详情，返回JSON", notes = "")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String greetingJson(@PathVariable("id") Long id) {
+    public String bookFindOne(@PathVariable("id") Long id) {
 
         Book book = new Book();
         book.setId(id);
@@ -29,6 +35,13 @@ public class HelloController {
         return JSONObject.toJSONString(book);
     }
 
+    @ApiOperation(value = "创建一本书", notes = "")
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public String bookCreate(@ApiParam(value = "创建一本书的postData", required = true) @Valid @RequestBody BookCreateRequest bookCreateRequest,
+                             @Context HttpServletRequest request) {
+
+        return JSONObject.toJSONString(bookCreateRequest);
+    }
 
     @ApiOperation(value = "返回字符串", notes = "")
     @GetMapping(value = "/string", produces = MediaType.APPLICATION_JSON_VALUE)
---
+++ src/main/java/com/fengsong97/spring/demo/request/BookCreateRequest.java
@@ -0,0 +1,13 @@
+package com.fengsong97.spring.demo.request;
+
+import lombok.Data;
+
+/**
+ * 创建人 fengsong
+ * 创建时间 2018/04/25 17:01
+ **/
+@Data
+public class BookCreateRequest {
+    private String name;
+    private String desciption;
+}

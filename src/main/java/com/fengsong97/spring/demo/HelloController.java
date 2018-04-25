db06deefc49b154ec0e2bcc5ca865f67b89c67e1
冯松 <fengsong97@qq.com>
2018-04-25 16:18:45 +0800

    @Controller --> @RestController

--- src/main/java/com/fengsong97/spring/demo/HelloController.java
+++ src/main/java/com/fengsong97/spring/demo/HelloController.java
@@ -1,17 +1,21 @@
 package com.fengsong97.spring.demo;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 创建人 fengsong
  * 创建时间 2018/04/25 15:17
  **/
-@Controller
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping(value = "/string", produces = MediaType.APPLICATION_JSON_VALUE)
     public String greeting() {
         return "Hello World";
     }
